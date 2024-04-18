889c3c563a89b8f0c361cbdf3da9378b381aa74a
Orko24 <adamaspayments0217@gmail.com>
2024-04-18 10:49:30 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
-const path = require('path')
+// const path = require('path')
  
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  }
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     domains: ['res.cloudinary.com', 'media.dev.to']
+//   }
+// }
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+ 
+module.exports = nextConfig;
