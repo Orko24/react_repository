519e485e59dd12628e5631eb10adcb69071842d4
Orkohelios <orko.bairagi@helioscta.com>
2024-11-13 18:03:58 -0700

    update

--- utils/data/skills_full.js
+++ utils/data/skills_full.js
@@ -1,7 +1,33 @@
 export const work_skills = [
     {
         id: 1,
-        Workplace: "ICE Process Management LLC.",
+        Workplace: "ICE Process Management LLC/ Bravura AI",
+        duration: "October 2024-Present",
+        Title: "Senior Artificial Intelligence Engineer",
+        Skills: 'SQLite, Python, NLP, Hugging Face LLMs, Lang-Chain,\
+        Chroma DB, SQL Alchemy, Web Application Design, Data Pipelines, \
+        OCR, Artificial Intelligence, Vector Databases and Embedded Vector Databases, \
+        R.A.G systems, Prompt Engineering, Chatbots, Pandas, NumPy, ChatGPT, \
+        Microsoft Co-pilot, Llama3, OpenAI, Data Science & Data Engineering, \
+        Python Package Creation, Agile development, FAISS, Data Classification, \
+        Text Classification, SQL, and NoSQL, Python architecture, version control, \
+        Git, Vector Databases, Data Quality assessment, unit tests, Azure Cloud Computing, \
+        Server Development, Azure Microservices, Microsoft Visio, FAST API, PyQT6, \
+        PowerShell Script, Encryption, Machine Code, Pip Package development. ' , 
+    },
+    {
+        id: 2,
+        Workplace: "Helios CTA",
+        duration: "July 2024-September 2024",
+        Title: "Software Engineer",
+        Skills: 'Investment Analysis, Commodity demand profile and supply chain analysis, \
+        Data Filtration, Software design and development, Market Analysis, Snowflake, PyQT6,\
+        Data Science, PowerShell, Azure, JSON, REST APIs, FAST API, Excel, Options, \
+        and Future Analysis.' , 
+    },
+    {
+        id: 3,
+        Workplace: "ICE Process Management LLC/ Bravura A",
         duration: "May 2024-present",
         Title: "Software & Artificial Intelligence Engineer",
         Skills: 'SQLite, Python, NLP, LLM, Lang-Chain, Chroma DB, \
@@ -11,7 +37,7 @@ export const work_skills = [
         Microsoft Co-pilot, Llama3, OpenAI ' , 
     },
     {
-        id: 2,
+        id: 4,
         Workplace: "ICE Process Management LLC.",
         duration: "April 2024-present",
         Title: "Software Consultant",
@@ -21,7 +47,7 @@ export const work_skills = [
     },
 
     {
-        id: 3,
+        id: 5,
         Workplace: "ScaleAI",
         duration: "Jan 2024-June 2024",
         Title: "AI Consultant",
@@ -33,7 +59,7 @@ export const work_skills = [
         PostgreSQL, Kaggle API integration, Hugging face API, PyTorch, Keras, TensorFlow', 
     },
     {
-        id: 4,
+        id: 6,
         Workplace: "IBM Startup Partner Program; Adamas Audio",
         duration: "June 2020 - Jan 2022",
         Title: "Lead Software Engineer and Software Architect",
@@ -44,7 +70,7 @@ export const work_skills = [
         Bash Script, Git, Cryptography, SSL, Cyber Security, Data Analysis, Data Science'
     },
     {
-        id: 5,
+        id: 7,
         Workplace: "Quant-connect",
         duration: "Jan 2022 - Jan 2024",
         Title: "Algorithmic Trader",
@@ -53,7 +79,7 @@ export const work_skills = [
         Sentiment Analysis, Data Analysis'
     },
     {
-        id: 6,
+        id: 8,
         Workplace: "Quant-connect",
         duration: "Jan 2022 - Jan 2024",
         Title: "Algorithmic Trader",
