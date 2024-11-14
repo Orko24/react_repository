f6123924ad62cb04fe949c34a8139e14cfb673fc
Orkohelios <orko.bairagi@helioscta.com>
2024-11-13 17:42:20 -0700

    update

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -6,43 +6,43 @@ export const experiences = [
     duration: "(October 2024 to Present)"
   },
   {
-    id: 0,
+    id: 1,
     title: 'Software Engineer',
     company: "Helios CTA",
     duration: "(July 2024 to September 2024)"
   },  
   {
-    id: 1,
+    id: 2,
     title: 'Software & Artificial Intelligence Engineer',
     company: "ICE Process Management LLC/ Bravura AI",
     duration: "(May 2024 to Present)"
   },
   {
-    id: 2,
+    id: 3,
     title: 'Software Consultant',
     company: "ICE Process Management LLC/ Bravura AI",
     duration: "(April 2024 to Present)"
   },
   {
-    id: 3,
+    id: 4,
     title: 'AI Consultant',
     company: "Scale AI: RemoteTasks",
     duration: "(Jan 2024 to June 2024)"
   },
   {
-    id: 4,
+    id: 5,
     title: "Lead Software Engineer and Software Architect",
     company: "IBM Startup Partner Program; Adamas Audio",
     duration: "(Jan 2022 to Jan 2024)"
   },
   {
-    id: 5,
+    id: 6,
     title: "Algorithmic Trader",
     company: "Quant-connect",
     duration: "(June 2020 to Jan 2022)"
   },
   {
-    id: 6,
+    id: 7,
     title: "Undergraduate Researcher",
     company: "University of Calgary",
     duration: "(Sept 2019 to June 2020)"
